7ff00202f10645b7333acd804f3456bd1612bb9c
spwitas <spwitas@gmail.com>
2025-01-23 21:14:37 +0000

    word count

--- main.py
+++ main.py
@@ -2,5 +2,15 @@ def main():
     with open("books/frankenstein.txt") as pth:
         file_contents = pth.read()
     print(file_contents)
-    
-main()
+
+def wordcount(str):
+    words = str.split()
+    word_count = 0
+    for word in words:
+        word_count += 1
+    print(word_count)
+    return(word_count)
+
+with open("books/frankenstein.txt") as pth:
+    file_contents = pth.read()
+    wordcount(file_contents)
