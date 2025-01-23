bbe70f6cfa50bf75941b0680ccef1df735d3e21b
spwitas <spwitas@gmail.com>
2025-01-23 20:48:55 +0000

    fixing even more bugs!

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def main(): 
-    with open("/home/noneofyourbusiness/workspace/github.com/Mullich/bookbot/books/frankenstein.txt") as pth:
+    with open("books/frankenstein.txt") as pth:
         file_contents = pth.read()
-
+    print(file_contents)
+    
 main()
