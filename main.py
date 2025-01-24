eaba74ff3cc39ec2855fc7dd896ce6cecac84cb6
spwitas <spwitas@gmail.com>
2025-01-24 21:30:38 +0000

    added character count sorting and final report

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 def main(): 
     path = "books/frankenstein.txt"
     book_text = get_text(path)
-    character_count = charactercount(book_text)
-    print(character_count)
+    num_words = word_count(book_text)
+    charactercount = character_count(book_text)
+    sortedletterlist = letters_only(charactercount)
 
-def wordcount(str):
+    print(f"--- Begin report of {path}---")
+    print(F"{num_words} words found in the document")
+    print()
+    for letter_dict in sortedletterlist:
+        print(f"The '{letter_dict['letter']}' character was found {letter_dict['occurrence']} times")
+    print()
+    print("--- End report ---")
+
+def word_count(str):
     words = str.split()
     return len(words)
 
@@ -12,11 +21,29 @@ def get_text(path):
     with open(path) as pth:
         return pth.read()
 
-def charactercount(str):
+def character_count(str):
     characters = {}
+    characters_list = []
     for character in str:
         lwr = character.lower()
-        characters[lwr] += 1
+        if lwr not in characters:
+            characters[lwr] = 1
+        else:
+            characters[lwr] += 1
     return characters
 
+def letters_only(chardict):
+    charlist = []
+    for key, value in chardict.items():
+        if key.isalpha():
+            temp_dict = {"letter": key, "occurrence": value}
+            charlist.append(temp_dict)
+    charlist.sort(reverse=True, key=sort_it)
+    return charlist
+
+def sort_it(list):
+    return list["occurrence"]
+
 main()
+
+
